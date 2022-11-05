3b9b906e55aa131e2d88e7240e28d1d9dcb212c8
Ivan <ivanogamer2019@gmail.com>
2022-11-05 18:22:07 +0100

    5.3.1

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-'use script'
+"use script"
 
-const addToDo = document.getElementById('add_to_do');
-const listToDo = document.getElementById('list_to_do');
-const toDoList = document.querySelectorAll('.todo')
-const addButton = document.querySelectorAll('.add_button');
-const container = document.querySelector('.container');
-const button1 = document.getElementById('add_button_1');
+const addToDo = document.getElementById("add_to_do")
+const listToDo = document.getElementById("list_to_do")
+const toDoList = document.querySelectorAll(".todo")
+const addButton = document.querySelectorAll(".add_button")
+const container = document.querySelector(".container")
+const button1 = document.getElementById("add_button_1")
 
-let idButton = 10;
-let counterNotes = 0;
-let counterDelete = 0;
+let idButton = 10
+let counterNotes = 0
+let counterDelete = 0
 // Preloader function
-const preloaderFunction = window.onload = function () {
-    let preloader = document.getElementById("preloader");
+const preloaderFunction = (window.onload = function () {
+    let preloader = document.getElementById("preloader")
     const func = () => {
-        container.classList.remove('display_none');
-    };
-    setTimeout(func, 3 * 1000);
-
-
-};
-
+        container.classList.remove("display_none")
+    }
+    setTimeout(func, 3 * 1000)
+})
 
 addToDo.onclick = function () {
-    listToDo.classList.add('z_index');
-    addToDo.classList.remove('z_index');
+    listToDo.classList.add("z_index")
+    addToDo.classList.remove("z_index")
 }
 listToDo.onclick = function () {
-    listToDo.classList.remove('z_index');
-    addToDo.classList.add('z_index');
+    listToDo.classList.remove("z_index")
+    addToDo.classList.add("z_index")
 }
 // function texrareaScrollHeight() {
 //     let textarea = document.querySelector('textarea');
@@ -42,12 +39,9 @@ listToDo.onclick = function () {
 // }
 
 function createNote(title, text) {
-    const noteElement = document.createElement('div');
+    const noteElement = document.createElement("div")
 
-
-
-
-    noteElement.classList.add('note');
+    noteElement.classList.add("note")
     noteElement.innerHTML = `
     <button class="add_button_note add_button" id="${idButton}">
     <img src="img/icon_plus.png" alt="" />
@@ -66,102 +60,88 @@ function createNote(title, text) {
     <textarea id="note_text_input" class="hidden textarea_text">${text}</textarea>
 
     `
-    const editBtn = noteElement.querySelector('.note_edit');
-    const deleteBtn = noteElement.querySelector('.note_delete');
-    const titleElement = noteElement.querySelector('#note_title');
-    const textElement = noteElement.querySelector('#note_text');
-    const titleInputElement = noteElement.querySelector('#note_title_input');
-    const textInputElement = noteElement.querySelector('#note_text_input');
-    let addButtonNote = noteElement.querySelector('.add_button_note');
-
-
+    const editBtn = noteElement.querySelector(".note_edit")
+    const deleteBtn = noteElement.querySelector(".note_delete")
+    const titleElement = noteElement.querySelector("#note_title")
+    const textElement = noteElement.querySelector("#note_text")
+    const titleInputElement = noteElement.querySelector("#note_title_input")
+    const textInputElement = noteElement.querySelector("#note_text_input")
+    let addButtonNote = noteElement.querySelector(".add_button_note")
 
     addButtonNote.onclick = function () {
-        const element = createNote("Заголовок", "Ваш текст", idButton);
-        counterNotes = counterNotes + 1;
+        const element = createNote("Заголовок", "Ваш текст", idButton)
+        counterNotes = counterNotes + 1
         for (let i = 0; i < toDoList.length; i++) {
-            let idButtonClass = idButton - 1;
-            toDoList[i].appendChild(element);
+            let idButtonClass = idButton - 1
+            toDoList[i].appendChild(element)
             if (idButtonClass >= 10) {
-                addButtonNote.classList.add('display_none');
+                addButtonNote.classList.add("display_none")
             }
-
         }
-        idButton = idButton + 1;
-
-
-
+        idButton = idButton + 1
     }
 
-
     // Function button edit notes
-    editBtn.addEventListener('click', (e) => {
-        const textarea = document.querySelector('.textarea_text');
-        titleElement.classList.toggle('hidden');
-        textElement.classList.toggle('hidden');
+    editBtn.addEventListener("click", (e) => {
+        const textarea = document.querySelector(".textarea_text")
+        titleElement.classList.toggle("hidden")
+        textElement.classList.toggle("hidden")
 
-        titleInputElement.classList.toggle('hidden');
-        textInputElement.classList.toggle('hidden');
+        titleInputElement.classList.toggle("hidden")
+        textInputElement.classList.toggle("hidden")
 
-        textarea.addEventListener('keyup', function () {
+        textarea.addEventListener("keyup", function () {
             if (this.scrollTop > 0) {
-                this.style.height = this.scrollHeight + "px";
+                this.style.height = this.scrollHeight + "px"
             }
-            console.log('click text area');
-        });
-        console.log(textarea);
-
-    });
+            console.log("click text area")
+        })
+        console.log(textarea)
+    })
     // Function delete notes
-    deleteBtn.addEventListener('click', (e) => {
-        noteElement.remove();
-        counterNotes = counterNotes - 1;
-        counterDelete = counterDelete + 1;
+    deleteBtn.addEventListener("click", (e) => {
+        noteElement.remove()
+        counterNotes = counterNotes - 1
+        counterDelete = counterDelete + 1
         if (counterNotes == 0) {
             for (let i = 0; i < addButton.length; i++) {
-                addButton[i].classList.remove('display_none');
-
+                addButton[i].classList.remove("display_none")
             }
         }
-        if ((Number(addButtonNote.id) + counterDelete) == idButton) {
-            console.log('click');
+        if (Number(addButtonNote.id) + counterDelete == idButton) {
+            console.log("click")
             // for (let i = idButton; i > addButtonNote.id; i--) {
             //     console.log(addButtonNote.id);
 
             // }
         }
-        console.log(addButtonNote.id);
-        console.log(idButton);
-        console.log(counterDelete);
-    });
+        console.log(addButtonNote.id)
+        console.log(idButton)
+        console.log(counterDelete)
+    })
 
-    titleInputElement.addEventListener('input', (e) => {
-        titleElement.innerText = e.target.value;
-    });
-    textInputElement.addEventListener('input', (e) => {
-        textElement.innerText = e.target.value;
-    });
+    titleInputElement.addEventListener("input", (e) => {
+        titleElement.innerText = e.target.value
+    })
+    textInputElement.addEventListener("input", (e) => {
+        textElement.innerText = e.target.value
+    })
 
-
-    return noteElement;
+    return noteElement
 }
 
 for (let i = 0; i < addButton.length; i++) {
-
-    addButton[i].addEventListener('click', (e) => {
-        const element = createNote("Заголовок", "Ваш текст", idButton);
+    addButton[i].addEventListener("click", (e) => {
+        const element = createNote("Заголовок", "Ваш текст", idButton)
         if (addButton[i].id === "add_button_0") {
-            addButton[i].classList.add('display_none');
-            toDoList[i].appendChild(element);
-
+            addButton[i].classList.add("display_none")
+            toDoList[i].appendChild(element)
         } else if (addButton[i].id === "add_button_1") {
-            toDoList[i].appendChild(element);
-            addButton[i].classList.add('display_none');
-            idButton = idButton + 1;
-            counterNotes = counterNotes + 1;
-            counterDelete = 0;
-
+            toDoList[i].appendChild(element)
+            addButton[i].classList.add("display_none")
+            idButton = idButton + 1
+            counterNotes = counterNotes + 1
+            counterDelete = 0
         }
     })
-};
-
+}
