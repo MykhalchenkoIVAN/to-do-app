7a7964ca38d2d0aad4c072b9974ae2e5f737d658
Ivan <ivanogamer2019@gmail.com>
2022-11-07 21:16:56 +0100

    5.3.4

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 "use script"
 
-const addToDo = document.getElementById("add_to_do");
-const listToDo = document.getElementById("list_to_do");
-const toDoList = document.querySelectorAll(".todo");
-const toDoList0 = document.querySelectorAll(".todo0");
-const addButton = document.querySelectorAll(".add_button");
-const container = document.querySelector(".container");
-const button1 = document.getElementById("add_button_1");
+const addToDo = document.getElementById("add_to_do")
+const listToDo = document.getElementById("list_to_do")
+const toDoList = document.querySelectorAll(".todo")
+const toDoList0 = document.querySelectorAll(".todo0")
+const addButton = document.querySelectorAll(".add_button")
+const container = document.querySelector(".container")
+const button1 = document.getElementById("add_button_1")
 
+let idButton = 0
+let counterNotes = 0
+let counterDelete = 0
 
-
-let idButton = 0;
-let counterNotes = 0;
-let counterDelete = 0;
-
-let buttonArray = [];
+let buttonArray = []
 // Preloader function
 const preloaderFunction = (window.onload = function () {
-    let preloader = document.getElementById("preloader");
+    let preloader = document.getElementById("preloader")
     const func = () => {
-        container.classList.remove("display_none");
+        container.classList.remove("display_none")
     }
-    setTimeout(func, 3 * 1000);
+    setTimeout(func, 3 * 1000)
 })
 
 addToDo.onclick = function () {
-    listToDo.classList.add("z_index");
-    addToDo.classList.remove("z_index");
+    listToDo.classList.add("z_index")
+    addToDo.classList.remove("z_index")
 }
 listToDo.onclick = function () {
-    listToDo.classList.remove("z_index");
-    addToDo.classList.add("z_index");
+    listToDo.classList.remove("z_index")
+    addToDo.classList.add("z_index")
 }
 
 function createNote(title, text) {
+    const noteElement = document.createElement("div")
 
-
-
-    const noteElement = document.createElement("div");
-
-    noteElement.classList.add("note");
+    noteElement.classList.add("note")
     noteElement.innerHTML = `
     <button class="add_button_note add_button" id="${idButton}">
     <img src="img/icon_plus.png" alt="" />
@@ -58,110 +53,85 @@ function createNote(title, text) {
     <textarea id="note_text_input_${idButton}" class="hidden textarea_text">${text}</textarea>
 
     `
-    const editBtn = noteElement.querySelectorAll(".note_edit");
-    const deleteBtn = noteElement.querySelectorAll(".note_delete");
-    const titleElement = noteElement.querySelector("#note_title");
-    const textElement = noteElement.querySelector("#note_text");
-    const titleInputElement = noteElement.querySelector("#note_title_input");
-    const textInputElement = noteElement.querySelectorAll(".textarea_text");
-    const addButtonNote = noteElement.querySelectorAll(".add_button_note");
-    const addButtonNoteDelete = noteElement.querySelector(".add_button_note");
-
-
+    const editBtn = noteElement.querySelectorAll(".note_edit")
+    const deleteBtn = noteElement.querySelectorAll(".note_delete")
+    const titleElement = noteElement.querySelector("#note_title")
+    const textElement = noteElement.querySelector("#note_text")
+    const titleInputElement = noteElement.querySelector("#note_title_input")
+    const textInputElement = noteElement.querySelectorAll(".textarea_text")
+    const addButtonNote = noteElement.querySelectorAll(".add_button_note")
+    const addButtonNoteDelete = noteElement.querySelector(".add_button_note")
 
     for (let i = 0; i < addButtonNote.length; i++) {
         addButtonNote[i].addEventListener("click", function () {
             const element = createNote("Заголовок", "Ваш текст", idButton)
             for (let i = 0; i < toDoList.length; i++) {
-                toDoList[i].appendChild(element);
+                toDoList[i].appendChild(element)
             }
-            addButtonNote[i].classList.add("display_none");
+            addButtonNote[i].classList.add("display_none")
         })
     }
     for (let i = 0; i < editBtn.length; i++) {
-
-        editBtn[i].addEventListener('click', function (e) {
-            console.log(e.target.getAttribute('id'));
-            titleElement.classList.toggle("hidden");
-            textElement.classList.toggle("hidden");
-            titleInputElement.classList.toggle("hidden");
-            textInputElement[i].classList.toggle("hidden");
-
+        editBtn[i].addEventListener("click", function (e) {
+            console.log(e.target.getAttribute("id"))
+            titleElement.classList.toggle("hidden")
+            textElement.classList.toggle("hidden")
+            titleInputElement.classList.toggle("hidden")
+            textInputElement[i].classList.toggle("hidden")
 
             textInputElement[i].addEventListener("keyup", function () {
                 if (this.scrollTop > 0) {
                     this.style.height = this.scrollHeight + "px"
                 }
             })
-
         })
     }
     // Function delete notes
     for (let i = 0; i < deleteBtn.length; i++) {
         deleteBtn[i].addEventListener("click", (e) => {
-            noteElement.remove();
-            counterNotes = counterNotes - 1;
-            counterDelete = counterDelete + 1;
-            let lastElem = buttonArray.pop()
-            console.log(lastElem);
+            noteElement.remove()
+            counterNotes = counterNotes - 1
+            counterDelete = counterDelete + 1
+            buttonArray.pop();
+            let l = buttonArray[buttonArray.length - 1]
+            if (buttonArray.length > 0) {
+                l.classList.remove("display_none")
+                console.log("click")
+            }
             if (counterNotes == 0) {
                 for (let i = 0; i < addButton.length; i++) {
-                    buttonArray = [];
-                    addButton[i].classList.remove("display_none");
+                    buttonArray = []
+                    addButton[i].classList.remove("display_none")
                 }
             }
         })
     }
-    // deleteBtn.addEventListener("click", (e) => {
-    //     e
-    //     noteElement.remove();
-    //     counterNotes = counterNotes - 1;
-    //     counterDelete = counterDelete + 1;
-    //     if (counterNotes == 0) {
-    //         for (let i = 0; i < addButton.length; i++) {
-    //             addButton[i].classList.remove("display_none")
-    //         }
-    //     }
-    //     if (addButtonNoteDelete.length > 0) {
-    //         let c = addButtonNoteDelete.length - 1;
-    //         addButtonNoteDelete[c].classList.remove("display_none");
-    //     }
-
-    // });
-
     titleInputElement.addEventListener("input", (e) => {
         titleElement.innerText = e.target.value
-    });
+    })
     for (let i = 0; i < textInputElement.length; i++) {
         textInputElement[i].addEventListener("input", (e) => {
             textElement.innerText = e.target.value
-        });
+        })
     }
 
-
-
     buttonArray.push(addButtonNoteDelete)
-    idButton++;
+    idButton++
+    counterNotes = counterNotes + 1
 
-    counterNotes = counterNotes + 1;
-    console.log(counterNotes);
-    console.log(buttonArray);
     return noteElement
-
 }
 
 for (let i = 0; i < addButton.length; i++) {
     addButton[i].addEventListener("click", (e) => {
-        idButton = 0;
-        console.log(idButton);
+        idButton = 0
+        console.log(idButton)
         const element = createNote("Заголовок", "Ваш текст", idButton)
         if (addButton[i].id === "add_button_0") {
         } else if (addButton[i].id === "add_button_1") {
-            toDoList[i].appendChild(element);
-            addButton[i].classList.add("display_none");
-            counterDelete = 0;
-
+            toDoList[i].appendChild(element)
+            addButton[i].classList.add("display_none")
+            counterDelete = 0
         }
     })
-
 }
